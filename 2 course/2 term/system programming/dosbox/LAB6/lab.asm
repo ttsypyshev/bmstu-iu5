909b83b616c33d503801c59e41a1d3fa252d1606
ttsypyshev <ttsypyshev01@gmail.com>
2024-05-08 07:33:40 +0300

    Updated system programming
    Added report 5, 6, 7

--- 2 course/2 term/system programming/dosbox/LAB6/lab.asm
+++ 2 course/2 term/system programming/dosbox/LAB6/lab.asm
@@ -5,7 +5,7 @@
 prgr segment
 	                       assume CS:prgr,DS:data
 
-	begin:                                                    	; Начало программы
+	begin:                                                  	; Начало программы
 	                       mov    AX,data
 	                       mov    ES,AX
 
@@ -23,7 +23,7 @@ prgr segment
 	                       push   ES
 	                       pop    DS
 	                       jmp    layer1
-	nope:                                                     	; Нет параметров
+	nope:                                                   	; Нет параметров
 	                       mov    AX,data
 	                       mov    DS,AX
 	                       mov    AH,09h
@@ -31,38 +31,38 @@ prgr segment
 	                       int    21h
 	                       jmp    exit_program
 
-	layer1:                                                   	; Первый слой проверки
+	layer1:                                                 	; Первый слой проверки
 	                       mov    SI,offset destination
 	                       mov    DI,offset tmp1
 	                       mov    CL,9
 	                       rep    movsb
-	check_first_1:                                            	; Проверка первого параметра
+	check_first_1:                                          	; Проверка первого параметра
 	                       mov    DI,offset parameter1_1
 	                       mov    SI,offset tmp1
 	                       mov    CL,9
 	                       repe   cmpsb
 	                       jne    check_first_2
 	                       jmp    layer2
-	check_first_2:                                            	; Проверка второго варианта первого параметра
+	check_first_2:                                          	; Проверка второго варианта первого параметра
 	                       mov    DI,offset parameter1_2
 	                       mov    SI,offset tmp1
 	                       mov    CL,9
 	                       repe   cmpsb
 	                       jne    print_error_1
 	                       jmp    layer2
-	print_error_1:                                            	; Вывод ошибки
+	print_error_1:                                          	; Вывод ошибки
 	                       mov    AH,09h
 	                       mov    DX,offset error_2
 	                       int    21h
 	                       jmp    exit_program
 
-	layer2:                                                   	; Второй слой проверки
+	layer2:                                                 	; Второй слой проверки
 	                       mov    SI,offset destination
 	                       add    SI,10
 	                       mov    DI,offset tmp2
 	                       mov    CL,1
 	                       rep    movsb
-	second_check_1:                                           	; Проверка второго параметра
+	second_check_1:                                         	; Проверка второго параметра
 	                       mov    DI,offset parameter2_1
 	                       mov    SI,offset tmp2
 	                       mov    CL,1
@@ -70,7 +70,7 @@ prgr segment
 	                       jne    second_check_2
 	                       mov    falg[1],1
 	                       jmp    third_check_2
-	second_check_2:                                           	; Проверка второго варианта второго параметра
+	second_check_2:                                         	; Проверка второго варианта второго параметра
 	                       mov    DI,offset parameter2_2
 	                       mov    SI,offset tmp2
 	                       mov    CL,1
@@ -78,7 +78,7 @@ prgr segment
 	                       jne    third_check_1
 	                       jmp    third_check_2
 
-	third_check_1:                                            	; Первая проверка третьего параметра
+	third_check_1:                                          	; Первая проверка третьего параметра
 	                       mov    SI,offset destination
 	                       add    SI,10
 	                       mov    DI,offset tmp3
@@ -90,7 +90,7 @@ prgr segment
 	                       repe   cmpsb
 	                       jne    print_error_3
 	                       jmp    output
-	third_check_2:                                            	; Вторая проверка третьего параметра
+	third_check_2:                                          	; Вторая проверка третьего параметра
 	                       mov    SI,offset destination
 	                       add    SI,12
 	                       mov    DI,offset tmp3
@@ -102,16 +102,16 @@ prgr segment
 	                       repe   cmpsb
 	                       jne    print_error_3
 	                       jmp    output
-	print_error_3:                                            	; Вывод ошибки третьего параметра
+	print_error_3:                                          	; Вывод ошибки третьего параметра
 	                       mov    AH,09h
 	                       mov    DX,offset error_3
 	                       int    21h
 	                       jmp    exit_program
 	
-	output:                                                   	; Вывод
+	output:                                                 	; Вывод
 	                       mov    AX,02h
 	                       int    10h
-	clear:                                                    	; Очистка
+	clear:                                                  	; Очистка
 	                       cmp    falg[1],1
 	                       jne    input_instructions_2
 	                       mov    AH,09h
@@ -123,7 +123,7 @@ prgr segment
 	                       int    21h
 	                       CALL   CLRF
 	                       CALL   CLRF
-	input_instructions:                                       	; Ввод инструкций
+	input_instructions:                                     	; Ввод инструкций
 	                       mov    AH,09h
 	                       mov    DX,offset info_text
 	                       int    21h
@@ -143,7 +143,7 @@ prgr segment
 	                       loop   input_instructions
 	                       jmp    greeting_message
 
-	input_instructions_2:                                     	; Ввод инструкций (вторая часть)
+	input_instructions_2:                                   	; Ввод инструкций (вторая часть)
 	                       mov    AH,09h
 	                       mov    DX,offset hlpno
 	                       int    21h
@@ -162,11 +162,11 @@ prgr segment
 	                       int    21h
 	                       loop   input_instructions_2
 
-	next_step:                                                	; Следующий шаг
+	next_step:                                              	; Следующий шаг
 	                       CALL   CLRF
 	                       CALL   CLRF
 
-	greeting_message:                                         	; Приветственное сообщение
+	greeting_message:                                       	; Приветственное сообщение
 	                       mov    AH,09h
 	                       mov    DX,offset parameter_line
 	                       int    21h
@@ -204,10 +204,10 @@ prgr segment
 	                       mov    DX,offset tmp2
 	                       jmp    print_third_parameter
 
-	print_second_parameter:                                   	; Вывод второго параметра
+	print_second_parameter:                                 	; Вывод второго параметра
 	                       mov    DX,offset scnd2
 
-	print_third_parameter:                                    	; Вывод третьего параметра
+	print_third_parameter:                                  	; Вывод третьего параметра
 	                       int    21h
 	                       CALL   CLRF
 	                       mov    AH,09h
@@ -237,7 +237,7 @@ prgr segment
 	                       mov    AX,02h
 	                       int    10h
 
-	exit_program:                                             	; Конец программы
+	exit_program:                                           	; Конец программы
 	                       mov    AX,4C00h
 	                       int    21h
 
@@ -260,31 +260,31 @@ GETCH endp
 prgr ends
 
 data segment
-	parameter_line     db 'Line of parameters are: $'
-	frst               db '+ first parameter is: $'
-	scnd1              db '+ second parameter is: $'
-	scnd2              db '- there is no second parameter $'
-	thrd               db '+ third parameter is: $'
-	falg               db 2 dup (0)
-	dashes             db 23 dup ('-')
-	exit_instruction   db 'Press any key to exit... $'
-	destination db 20 dup ('#')
-	error_1            db 'There are no paremeters$'
-	error_2            db 'Error of the first parameter$'
-	error_3            db 'Access denied$'
-	parameter1_1       db 'Tsypyshev'
-	parameter1_2       db 'tsypyshev'
-	parameter2_1       db 'y'
-	parameter2_2       db 'n'
-	tmp1               db 9 dup ('1')
-	tmp2               db 1 dup ('2')
-	tmp3               db 9 dup ('3')
-	password           db '1234'
-	help_text_1        db 'This program just shows, how to use launch-parameters$'
-	help_text_2        db 'created by Ivanov Ivan, student of IU5-41 group, var.20$'
-	info_text          db 'Press * to continue $'
-	stars              db 9 dup ('*')
-	hlpno              db 'There is no spoon... Press ENTER $'
+	parameter_line   db 'Line of parameters are: $'
+	frst             db '+ first parameter is: $'
+	scnd1            db '+ second parameter is: $'
+	scnd2            db '- there is no second parameter $'
+	thrd             db '+ third parameter is: $'
+	falg             db 2 dup (0)
+	dashes           db 23 dup ('-')
+	exit_instruction db 'Press any key to exit... $'
+	destination      db 20 dup ('#')
+	error_1          db 'There are no paremeters$'
+	error_2          db 'Error of the first parameter$'
+	error_3          db 'Access denied$'
+	parameter1_1     db 'Tsypyshev'
+	parameter1_2     db 'tsypyshev'
+	parameter2_1     db 'y'
+	parameter2_2     db 'n'
+	tmp1             db 9 dup ('1')
+	tmp2             db 1 dup ('2')
+	tmp3             db 9 dup ('3')
+	password         db '1234'
+	help_text_1      db 'This program just shows, how to use launch-parameters$'
+	help_text_2      db 'created by Ivanov Ivan, student of IU5-41 group, var.20$'
+	info_text        db 'Press * to continue $'
+	stars            db 9 dup ('*')
+	hlpno            db 'There is no spoon... Press ENTER $'
 
 data ends
 

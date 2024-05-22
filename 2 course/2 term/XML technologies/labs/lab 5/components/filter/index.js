c0dcb139f806cfc254485cc6601b527275deb456
ttsypyshev <ttsypyshev01@gmail.com>
2024-05-22 23:29:02 +0300

    Updated XML technologies
    Added lab 5

---
+++ 2 course/2 term/XML technologies/labs/lab 5/components/filter/index.js
@@ -0,0 +1,36 @@
+export class FilterComponent {
+    constructor(parent) {
+        this.parent = parent;
+    }
+
+    getHTML() {
+        return (
+            `
+                <div class="form-group" style="width: 700px; margin-top: 10px; margin-left: 10px;">
+                    <label for="filter-select">Фильтр:</label>
+                    <select class="form-control" id="filter-select">
+                        <option value="">Все</option>
+                        <option value="friends">Друзья</option>
+                        <option value="unsure">Пользователи, которые выбрали «Возможно пойду» </option>
+                        <option value="managers">Менеджеры</option>
+                        <option value="donut">Пользователи, у которых есть платная подписка VK Donut</option>
+                    </select>
+                </div>
+            `
+        )
+    }
+
+
+    addListeners(listener) {
+        const select = document.getElementById('filter-select');
+        select.addEventListener('change', () => {
+            listener(select.value);
+        });
+    }
+
+    render(listener) {
+        const html = this.getHTML();
+        this.parent.insertAdjacentHTML('beforeend', html);
+        this.addListeners(listener);
+    }
+}

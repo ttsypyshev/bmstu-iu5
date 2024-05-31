be1fceb1371efa520e9981ca250a6ab1f7175cf6
ttsypyshev <ttsypyshev01@gmail.com>
2024-05-31 18:20:08 +0300

    Updated database
    Add lab 4

---
+++ 2 course/2 term/database/labs/lab4/lab4/mainwindow.h
@@ -0,0 +1,31 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QTableWidget>
+#include <QMessageBox>
+#include <QSqlError>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    QSqlDatabase dbconn;
+private slots:
+    void dbconnect();
+    void selectAll();
+    void add();
+    void del();
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
